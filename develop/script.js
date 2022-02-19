72c8cb644d937172aa20718bd8786f2b0e105cae
JamesH <jwhusc7@gmail.com>
2022-02-19 15:45:10 -0500

    added psuedo code for better randomize logic

--- develop/script.js
+++ develop/script.js
@@ -23,16 +23,14 @@ var customObject = {
   }
 }
 
-//math function that randomized characters
-function random(AllPossPassword) {
-  return Math.floor(Math.random() * AllPossPassword.length)
-}
-
 // garuntee at least one of slection char to be built in
 function generatePassword() {
   // Shuffle arrays then add to AllPossPassword
-  var AllPossPassword = ""
-  var ActPassword = ""
+  var AllPossPassword = "";
+  var ActPassword = "";
+  var valid = false;
+  // divide chosen strings to input evenly
+
   if (customObject.special) {
     AllPossPassword = AllPossPassword + special
   };
@@ -45,12 +43,43 @@ function generatePassword() {
   if (customObject.alphaUp) {
     AllPossPassword = AllPossPassword + alphaUp
   };
+
+    // loop through each selection seperately
+    // gathers random characters from each selection 
+    // then concatenate together
   for (i = 0; i < customObject.lengthSelect; i++){
-    var ranNum = random(AllPossPassword);
-    var ranChar = AllPossPassword.charAt(ranNum);
-    ActPassword = ActPassword + ranChar;
+    // switch
+    //  case (customObject.special):
+    //    var notMatched = true
+    //    while (notMatch){
+    //      if (AllPossPassword[Math.floor(Math.random() * AllPossPassword.length)].match("/W")){
+    //           
+    //      };
+    //    }
+    //  case(customObject.numeric):
+    //    var notMatched = true
+    //    while (notMatch){
+    //      if (AllPossPassword[Math.floor(Math.random() * AllPossPassword.length)].match("/D")){
+    //           
+    //      };
+    //    }
+
+    ActPassword = AllPossPassword[Math.floor(Math.random() * AllPossPassword.length)];
+    // test if special char matches at any index
+    console.log(ActPassword[ActPassword.search("\W")])
+    console.log(ActPassword[ActPassword.search(special)])
+    if (ActPassword[ActPassword.search(special)] === //search through special array for that character ) {
+      valid = true;
+      console.log(valid)
+    }
   };
-  return ActPassword
+
+  // check function
+  // if customObject.special === true then check actpassword has 1 special character
+  // 
+  if (valid){
+    return ActPassword
+  }
 }
 
 // Write AllPossPassword to the #AllPossPassword input
